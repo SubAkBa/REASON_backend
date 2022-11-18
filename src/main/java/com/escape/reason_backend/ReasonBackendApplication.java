88e58c9e56f89591fe1851e4902359395e7c1540
SubAkBa <circle5926@gmail.com>
2022-11-18 23:22:29 +0900

    initial commit

---
+++ src/main/java/com/escape/reason_backend/ReasonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.escape.reason_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReasonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReasonBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escape/reason_backend/ReasonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escape.reason_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReasonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
